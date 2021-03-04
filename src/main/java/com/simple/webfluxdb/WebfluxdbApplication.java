8e95de5f58822a5846c4f89ea8aa4272d1546fb1
mohitjain@optum.com <mohitjain@optum.com>
2021-03-04 12:27:03 +0530

    first commit

---
+++ src/main/java/com/simple/webfluxdb/WebfluxdbApplication.java
@@ -0,0 +1,13 @@
+package com.simple.webfluxdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simple/webfluxdb/WebfluxdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.webfluxdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
